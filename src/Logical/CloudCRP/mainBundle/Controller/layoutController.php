ef616779093078270c4808dbddaad603656a50c2
Vladimir Hidalgo <vladimiroski@gmail.com>
2015-01-18 21:50:44 -0600

    No mostrar menu si no se ha seleccionado empresa

--- src/Logical/CloudCRP/mainBundle/Controller/layoutController.php
+++ src/Logical/CloudCRP/mainBundle/Controller/layoutController.php
@@ -7,9 +7,15 @@ use Symfony\Component\HttpFoundation\Request;
 
 class layoutController extends Controller
 {
-    public function mostrarNombreEmpresaAction(Request $request) {
+    public function mostrarNombreEmpresaAction(Request $request)
+    {
         $sEmpresa = $this->get('session')->get('global_empresa');
         
         return new \Symfony\Component\HttpFoundation\Response(@$sEmpresa[0]['nombre'] ?: 'Ninguna empresa seleccionada.');
+    }
+    
+    public function empresaEstablecidaAction(Request $request)
+    {
+        return new \Symfony\Component\HttpFoundation\Response($this->get('session')->get('global_empresa_id'));
     }
 }
