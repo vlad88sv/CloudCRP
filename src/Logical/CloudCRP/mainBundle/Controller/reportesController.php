69b776d9dce24bb95301169946e1d915280a4ac9
Vladimir Hidalgo <vladimiroski@gmail.com>
2015-01-25 12:46:49 -0600

    Bases para la creacion de reportes

--- src/Logical/CloudCRP/mainBundle/Controller/reportesController.php
+++ src/Logical/CloudCRP/mainBundle/Controller/reportesController.php
@@ -7,11 +7,69 @@ use Symfony\Component\HttpFoundation\Request;
 use \Logical\CloudCRP\mainBundle\Entity\transacciones;
 use \Logical\CloudCRP\mainBundle\Entity\partidas;
 use \Symfony\Component\HttpFoundation\JsonResponse;
+use \Symfony\Component\HttpFoundation\Response;
 
 class reportesController extends Controller {
+    
+    private $data = array();
 
-    public function mostrarAction(Request $request) {
+    public function mostrarAction(Request $request, $id) {
+        $render = null;
+        
+        $this->data['fechaInicio'] = $request->get('txtFechaInicial', date('Y-m-01'));
+        $this->data['fechaFinal'] = $request->get('txtFechaFinal', date('Y-m-t'));
 
-        return $this->render('LCCMainBundle:partida:new.html.twig', array('data' => $data));
+
+        switch ($id) {
+            case '1':
+                // Libro auxiliar del mayor
+                $render = $this->reporte1();
+                break;
+            case '2':
+                // Partida de diario
+                $render = $this->reporte2();
+                break;
+            case '3':
+                 // Balance de comprobación
+                $render = $this->reporte3();
+                break;
+            default:
+                $render = $this->renderView('LCCMainBundle:reportes:indiceReportes.html.twig', array('data' => $this->data));
+        }
+        
+        return new Response($render);
     }
+    
+    private function vistaHtml($aReporte) {
+        
+    }
+    
+    private function vistaPdf($aReporte) {
+        
+    }
+    
+    private function vistaXls($aReporte) {
+        
+    }
+
+    // Libro auxiliar del mayor
+    private function reporte1() {
+        // Este reporte contiene el resumen por cada cuenta de tipo mayor
+        $em = $this->getDoctrine()->getManager();
+        
+        $result = $em->createQuery('SELECT * FROM LCCMainBundle:partidas WHERE fecha >= :fechaInicial AND fecha <= :fechaFinal');
+        
+        return $this->renderView('LCCMainBundle:reportes:libroAuxiliarDelMayor.html.twig', array('data' => $this->data));
+    }
+
+    // Partida de diario
+    private function reporte2() {
+        return $this->renderView('LCCMainBundle:reportes:generico.html.twig', array('data' => $this->data));
+    }
+
+    // Balance de comprobación
+    private function reporte3() {
+        return $this->renderView('LCCMainBundle:reportes:generico.html.twig', array('data' => $this->data));
+    }
+
 }
